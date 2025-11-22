ae2f55ba164da68c0deaaf05b61adab38d02fca8
Christian Strommen <cpstrommen@gmail.com>
2025-11-21 22:47:00 -0600

    flight map initial test

---
+++ js/flight-map-data.js
@@ -0,0 +1,109 @@
+// Each object is one flight leg.
+// type: "pic" for flights you flew, "commercial" for airline flights.
+
+window.flightLegs = [
+  {
+    origin: "KOEO",
+    originName: "L O Simenstad Municipal Airport",
+    originLat: 45.3086,
+    originLng: -92.6903,
+
+    dest: "KEAU",
+    destName: "Eau Claire Regional Airport",
+    destLat: 44.8658,
+    destLng: -91.4843,
+
+    date: "2019-9-14",
+    type: "pic",
+    notes: "N149AC"
+  },
+  {
+    origin: "KEAU",
+    originName: "Eau Claire Regional Airport",
+    originLat: 44.8658,
+    originLng: -91.4843,
+    
+    dest: "KOEO",
+    destName: "L O Simenstad Municipal Airport",
+    destLat: 45.3086,
+    destLng: -92.6903,
+
+    date: "2019-9-14",
+    type: "pic",
+  },
+  {
+    origin: "KEAU",
+    originName: "Eau Claire Regional Airport",
+    originLat: 44.8658,
+    originLng: -91.4843,
+    
+    dest: "KOEO",
+    destName: "L O Simenstad Municipal Airport",
+    destLat: 45.3086,
+    destLng: -92.6903,
+
+    date: "2019-9-14",
+    type: "pic",
+    notes: "N149AC"
+  },
+  {
+    origin: "KOEO",
+    originName: "L O Simenstad Municipal Airport",
+    originLat: 45.3086,
+    originLng: -92.6903,
+    
+    dest: "KSSQ",
+    destName: "Shell Lake Airport",
+    destLat: 45.7422,
+    destLng: -91.9205,
+
+    date: "2019-10-20",
+    type: "pic",
+    notes: "N149AC"
+  },
+  {
+    origin: "KSSQ",
+    originName: "Shell Lake Airport",
+    originLat: 45.7422,
+    originLng: -91.9205,
+    
+    dest: "KOEO",
+    destName: "L O Simenstad Municipal Airport",
+    destLat: 45.3086,
+    destLng: -92.6903,
+    
+    date: "2019-10-20",
+    type: "pic",
+    notes: "N149AC"
+  },
+  {
+    origin: "KOEO",
+    originName: "L O Simenstad Municipal Airport",
+    originLat: 45.3086,
+    originLng: -92.6903,
+    
+    dest: "KSYN",
+    destName: "Stanton Airfield",
+    destLat: 44.4692,
+    destLng: -93.0164,
+
+    date: "2019-11-23",
+    type: "pic",
+    notes: "N149AC"
+  },
+  {
+    origin: "KSYN",
+    originName: "Stanton Airfield",
+    originLat: 44.4692,
+    originLng: -93.0164,
+    
+    dest: "KOEO",
+    destName: "L O Simenstad Municipal Airport",
+    destLat: 45.3086,
+    destLng: -92.6903,
+    
+    date: "2019-11-23",
+    type: "pic",
+    notes: "N149AC"
+  }
+];
